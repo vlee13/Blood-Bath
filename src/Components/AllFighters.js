bbd9b326e8b5b1ee11fbca54fd81a26252bd255f
vlee13 <vlee13@gmail.com>
2021-07-20 19:30:48 -0400

    all fighters loaded

---
+++ src/Components/AllFighters.js
@@ -0,0 +1,190 @@
+import React from "react";
+import lycan18 from "../images/lycan18.gif";
+import LYCAN01 from "../images/LYCAN01.gif";
+import LYCAN02 from "../images/LYCAN02.gif";
+import LYCAN06 from "../images/LYCAN06.gif";
+import LYCAN11 from "../images/LYCAN11.gif";
+import LYCAN12 from "../images/LYCAN12.gif";
+import LYCAN13 from "../images/LYCAN13.gif";
+import LYCAN14 from "../images/LYCAN14.gif";
+import LYCAN16 from "../images/LYCAN16.gif";
+import LYCAN17 from "../images/LYCAN17.gif";
+import LYCAN20 from "../images/LYCAN20.gif";
+import LYCAN22 from "../images/LYCAN22.gif";
+import LYCAN25 from "../images/LYCAN25.gif";
+import LYCAN29 from "../images/LYCAN29.gif";
+import LYCAN34 from "../images/LYCAN34.gif";
+import LYCANKING from "../images/LYCANKING.gif";
+import DF21 from "../images/DF21.gif";
+import DF33 from "../images/DF33.gif";
+import DF56 from "../images/DF56.gif";
+import DF77 from "../images/DF77.gif";
+import DF81 from "../images/DF81.gif";
+import DF87 from "../images/DF21.gif";
+import DF92 from "../images/DF92.gif";
+import DF94 from "../images/DF94.gif";
+import DF99 from "../images/DF99.gif";
+import DFWARG from "../images/DFWARG.gif";
+import GLOW from "../images/GLOW.gif";
+
+const AllFighters = () => {
+  return (
+    <div className="fighters-container">
+      <div className="lycan-container">
+        <h2 style={{ color: "red" }}>LYCANS</h2>
+        <div>
+          <img src={lycan18} alt="lycan18" />
+          <h3>EROWID</h3>
+        </div>
+        <div>
+          <img src={LYCAN29} alt="lycan18" />
+          <h3>DEADTAPES</h3>
+        </div>
+        <div>
+          <img src={LYCAN22} alt="lycan18" />
+          <h3>BUYHIGH</h3>
+        </div>
+        <div>
+          <img src={LYCAN20} alt="lycan18" />
+          <h3>DORTHFADER</h3>
+        </div>
+        <div>
+          <img src={LYCAN16} alt="lycan18" />
+          <h3>ALB3RTUO</h3>
+        </div>
+        <div>
+          <img src={LYCAN34} alt="lycan18" />
+          <h3>BEAN</h3>
+        </div>
+        <div>
+          <img src={LYCAN14} alt="lycan18" />
+          <h3>CH3XICAN</h3>
+        </div>
+        <div>
+          <img src={LYCAN13} alt="lycan18" />
+          <h3>DEADJED</h3>
+        </div>
+        <div>
+          <img src={LYCAN06} alt="lycan18" />
+          <h3>UTZ</h3>
+        </div>
+        <div>
+          <img src={LYCAN02} alt="lycan18" />
+          <h3>LYCANMORI</h3>
+        </div>
+        <div>
+          <img src={LYCAN11} alt="lycan18" />
+          <h3>FORGE</h3>
+        </div>
+        <div>
+          <img src={LYCAN12} alt="lycan18" />
+          <h3>SIDDLULLA</h3>
+        </div>
+        <div>
+          <img src={LYCAN01} alt="lycan18" />
+          <h3>YVKI</h3>
+        </div>
+        <div>
+          <img src={LYCAN25} alt="lycan18" />
+          <h3>LIZARD</h3>
+        </div>
+        <div>
+          <img src={LYCAN17} alt="lycan18" />
+          <h3>DEEPIKA</h3>
+        </div>
+        <div>
+          <img src={LYCANKING} alt="lycan18" />
+          <h3>FARMERJOE</h3>
+        </div>
+      </div>
+      <div>
+        <h2 style={{ marginBottom: "35px" }}>ROUNDS</h2>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+        <div>7</div>
+        <div>8</div>
+        <div>9</div>
+        <div>10</div>
+        <div>11</div>
+        <div>12</div>
+        <div>13</div>
+        <div>14</div>
+        <div>15</div>
+        <div>16</div>
+      </div>
+      <div className="df-container">
+        <h2 style={{ color: "red" }}>DEAD FREAKS</h2>
+        <div>
+          <h3>GLEPASTEUR</h3>
+          <img src={DF21} alt="lycan18" />
+        </div>
+        <div>
+          <h3>BALLENTERPZ</h3>
+          <img src={DFWARG} alt="lycan18" />
+        </div>
+        <div>
+          <h3>MACNUTZ</h3>
+          <img src={GLOW} alt="lycan18" />
+        </div>
+        <div>
+          <h3>SNKRZ93</h3>
+          <img src={GLOW} alt="lycan18" />
+        </div>
+        <div>
+          <h3>ADVMHIRSCH</h3>
+          <img src={DF92} alt="lycan18" />
+        </div>
+        <div>
+          <h3>BUFFALOSPACE</h3>
+          <img src={DF33} alt="lycan18" />
+        </div>
+        <div>
+          <h3>RAINDROP</h3>
+          <img src={DF87} alt="lycan18" />
+        </div>
+        <div>
+          <h3>JENNIFER</h3>
+          <img src={DF94} alt="lycan18" />
+        </div>
+        <div>
+          <h3>AGRO23</h3>
+          <img src={DF56} alt="lycan18" />
+        </div>
+        <div>
+          <h3>YANK</h3>
+          <img src={DF77} alt="lycan18" />
+        </div>
+        <div>
+          <h3>NONNY</h3>
+          <img src={DF99} alt="lycan18" />
+        </div>
+        <div>
+          <h3>BOSTONBOB</h3>
+          <img src={DF81} alt="lycan18" />
+        </div>
+        <div>
+          <h3>ROVA</h3>
+          <img src={GLOW} alt="lycan18" />
+        </div>
+        <div>
+          <h3>REBELSTARR888</h3>
+          <img src={GLOW} alt="lycan18" />
+        </div>
+        <div>
+          <h3>BENG</h3>
+          <img src={GLOW} alt="lycan18" />
+        </div>
+        <div>
+          <h3>WOKE.GHOSTBABY</h3>
+          <img src={GLOW} alt="lycan18" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AllFighters;
